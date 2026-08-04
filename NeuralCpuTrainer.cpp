9f3f9a980305d5c8e48a851a872e46e0b79fe917
Mike Oliphant <oliphant@nostatic.org>
2026-08-04 08:39:36 -0700

    WaveNet layer working

--- NeuralCpuTrainer.cpp
+++ NeuralCpuTrainer.cpp
@@ -6,90 +6,266 @@
 #include "WaveNet.h"
 #include "WaveNetBackprop.h"
 
+#define MAX_SAMPLES 8192
+
 using namespace NeuralAudio;
 using namespace NeuralCpuTrain;
 
-int main()
+static void TestModel(BackpropModelT<float, 1, 1>& modelBackprop)
 {
-	const size_t totalSamples = 48000;
-	const size_t blockSize = 64; // model processes 64 samples at a time
-	const size_t numBlocks = totalSamples / blockSize;
+	const size_t totalSamples = MAX_SAMPLES;
 
 	// Create long input (sine) and target (identity for now)
-	std::vector<float> input(totalSamples);
-	std::vector<float> target(totalSamples);
+	ChannelBuffer<float, 1, totalSamples> input;
+	ChannelBuffer<float, 1, totalSamples> target;
 
 	for (size_t i = 0; i < totalSamples; ++i)
 	{
-		input[i] = std::sin(static_cast<float>(i) * 0.01f);
-		target[i] = input[i]; // identity target for now
+		input(0, i) = std::sin(static_cast<float>(i) * 0.01f);
+		target(0, i) = input(0, i); // identity target for now
 	}
 
-	DenseLayerT<float, 1, 1, false> model;
-	DenseBackpropT<float, 1, 1, false> modelBackprop(model);
-
 	// Initialize weights
-	size_t nWeights = model.GetNumWeights();
-	std::vector<float> weights(nWeights);
-	std::mt19937 rng(123);
-	std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
-	for (auto& w : weights) w = dist(rng);
+	//size_t nWeights = modelBackprop.GetNumWeights();
+	//std::vector<float> weights(nWeights);
+	//std::mt19937 rng(123);
+	//std::uniform_real_distribution<float> dist(-0.1f, 0.1f);
+	//for (auto& w : weights) w = dist(rng);
 
-	auto it = weights.begin();
+	//auto it = weights.begin();
 
-	model.SetWeights(it);
+	//modelBackprop.SetWeights(it);
 
-	for (int iter = 0; iter < 200; ++iter)
+	modelBackprop.RandomizeWeights();
+
+	size_t receptiveField = modelBackprop.GetReceptiveField();
+	size_t validSampleCount = totalSamples - receptiveField;
+
+	ChannelBuffer<float, 1, totalSamples> forwardOutput;
+	ChannelBuffer<float, 1, totalSamples> outputGradient;
+	ChannelBuffer<float, 1, totalSamples> layerOutputGradient;
+
+	for (int iter = 0; iter < 2000; ++iter)
 	{
-		ChannelBuffer<float, 1, blockSize> blockIn;
-		ChannelBuffer<float, 1, blockSize> blockOut;
-		ChannelBuffer<float, 1, blockSize> blockTarget;
-		ChannelBuffer<float, 1, blockSize> blockGradient;
-		ChannelBuffer<float, 1, blockSize> blockOutGradient;
+		forwardOutput.SetZero();
 
-		DenseBackpropWeightsT<float, 1, 1, false> weightAccumulator;
+		modelBackprop.Reset();
 
-		weightAccumulator.SetZero();
+		modelBackprop.Forward(input, forwardOutput);
 
-		for (size_t b = 0; b < numBlocks; ++b)
-		{
-			std::memcpy(blockIn.GetData(), input.data() + b * blockSize, blockSize * sizeof(float));
-			std::memcpy(blockTarget.GetData(), target.data() + b * blockSize, blockSize * sizeof(float));
-
-			model.Process(blockIn, blockOut);
-
-			auto map = blockGradient.GetEigenMap();
-
-			map = 2.0f * (blockOut.GetEigenMapConst().array() - blockTarget.GetEigenMapConst().array());
-
-			modelBackprop.Backward(blockIn, blockGradient, blockOutGradient, weightAccumulator);
-		}
-
-		weightAccumulator.Divide(static_cast<float>(numBlocks * blockSize));
-
-		weightAccumulator.ApplyGradients(model);
+		auto map = outputGradient.GetEigenMap();
 
 		double totErr = 0.0;
 
-		for (size_t b = 0; b < numBlocks; ++b)
+		for (size_t i = 0; i < totalSamples; i++)
 		{
-			std::memcpy(blockIn.GetData(), input.data() + b * blockSize, blockSize * sizeof(float));
-			std::memcpy(blockTarget.GetData(), target.data() + b * blockSize, blockSize * sizeof(float));
-
-			model.Process(blockIn, blockOut);
-
-			for (int i = 0; i < blockSize; i++)
+			if (i < receptiveField)
 			{
-				double diff = blockOut(0, i) - blockIn(0, i);
+				map(i) = 0.0f;
+			}
+			else
+			{
+				float diff = forwardOutput(0, i) - input(0, i);
+
+				map(i) = 2.0f * diff;
 
 				totErr += diff * diff;
 			}
 		}
 
-		float blah = model.GetWeights()(0, 0);
+		std::cout << "Iter: " << iter << " MSE: " << (totErr / static_cast<float>(validSampleCount)) << std::endl;
+		
+		modelBackprop.Backward(input, outputGradient, layerOutputGradient);
 
-		std::cout << "Iter: " << iter << " MSE: " << (totErr / static_cast<float>(numBlocks * blockSize)) << std::endl;
+		modelBackprop.DivideWeights(static_cast<float>((float)validSampleCount));
+
+		modelBackprop.ApplyGradients(0.05f);
 	}
+}
+
+template <typename T, int ConditionSize, int Channels, int KernelSize, int Dilation>
+class WaveNetLayerBackpropT : public BackpropModelT<T, Channels, Channels>
+{
+public:
+	void Forward(const ChannelRowSpan<T, Channels>& input, const ChannelRowSpan<T, ConditionSize>& condition, const ChannelRowSpan<T, Channels>& output, const ChannelRowSpan<T, Channels>& headOutput)
+	{
+		conv.Forward(input, convOut);
+
+		conditionMixIn.Forward(condition, conditionMixInOut);
+
+		auto convOutMap = convOut.GetEigenMap();
+		convOutMap.noalias() += conditionMixInOut.GetEigenMapConst();
+
+		relu.Forward(convOut, reluOut);
+
+		auto headOutputMap = headOutput.GetEigenMap();
+		headOutputMap.noalias() += reluOut.GetEigenMapConst();
+
+		oneByOne.Forward(reluOut, output);
+	}
+
+	void Backward(const ChannelRowSpan<T, Channels>& input, const ChannelRowSpan<T, ConditionSize>& condition, const ChannelRowSpan<T, Channels>& dOutput, const ChannelRowSpan<T, Channels>& dHeadOutput, const ChannelRowSpan<T, Channels>& dInput)
+	{
+		oneByOne.Backward(reluOut, dOutput, dOneByOneOut);
+
+		auto dOneByOneOutMap = dOneByOneOut.GetEigenMap();
+		dOneByOneOutMap.noalias() += dHeadOutput.GetEigenMapConst();
+
+		relu.Backward(convOut, dOneByOneOut, dReluOut);
+		conditionMixIn.Backward(condition, dReluOut, dInput);	// dInput just used for temp storage, will be overwritten
+		conv.Backward(input, dReluOut, dInput);
+	}
+
+	size_t GetReceptiveField() override
+	{
+		return conv.GetReceptiveField();
+	}
+
+	size_t GetNumWeights() override
+	{
+		return conv.GetNumWeights() + oneByOne.GetNumWeights() + conditionMixIn.GetNumWeights();;
+	}
+
+	void RandomizeWeights() override
+	{
+		conv.RandomizeWeights();
+		oneByOne.RandomizeWeights();
+		conditionMixIn.RandomizeWeights();
+	}
+
+	void SetWeights(std::vector<float>::iterator& inWeights) override
+	{
+		conv.SetWeights(inWeights);
+		conditionMixIn.SetWeights(inWeights);
+		oneByOne.SetWeights(inWeights);
+	}
+
+	void Reset() override
+	{
+		conv.Reset();
+		convOut.SetZero();
+		conditionMixIn.Reset();
+		conditionMixInOut.SetZero();
+		reluOut.SetZero();
+		oneByOne.Reset();
+		dOneByOneOut.SetZero();
+		dReluOut.SetZero();
+	}
+
+	void DivideWeights(float divisor) override
+	{
+		conv.DivideWeights(divisor);
+		oneByOne.DivideWeights(divisor);
+	}
+
+	void ApplyGradients(float scale) override
+	{
+		conv.ApplyGradients(scale);
+		oneByOne.ApplyGradients(scale);
+	}
+
+private:
+	Conv1DBackpropT<T, Channels, Channels, KernelSize, true, Dilation> conv;
+	ChannelBuffer<T, Channels, MAX_SAMPLES> convOut;
+	DenseBackpropT<T, ConditionSize, Channels, false> conditionMixIn;
+	ChannelBuffer<T, Channels, MAX_SAMPLES> conditionMixInOut;
+	LeakyReLUT<T, Channels> relu;
+	ChannelBuffer<T, Channels, MAX_SAMPLES> reluOut;
+	DenseBackpropT<T, Channels, Channels, true> oneByOne;
+	ChannelBuffer<T, Channels, MAX_SAMPLES> dOneByOneOut;
+	ChannelBuffer<T, Channels, MAX_SAMPLES> dReluOut;
+};
+
+
+template <typename T, int Channels>
+class A2BackpropT : public BackpropModelT<T, Channels, Channels>
+{
+	public:
+		void Forward(const ChannelRowSpan<T, Channels>& input, const ChannelRowSpan<T, Channels>& output) override
+		{
+			headOutput.SetZero();
+
+			layer1.Forward(input, input, layer1Out, headOutput);
+			layer2.Forward(layer1Out, input, output, headOutput);
+
+			auto outputMap = output.GetEigenMap();
+			outputMap.noalias() += headOutput.GetEigenMapConst();
+		}
+
+		void Backward(const ChannelRowSpan<T, Channels>& input, const ChannelRowSpan<T, Channels>& dOutput, const ChannelRowSpan<T, Channels>& dInput) override
+		{
+			layer2.Backward(layer1Out, input, dOutput, dOutput, dLayer1Out);
+			layer1.Backward(input, input, dLayer1Out, dOutput, dInput);
+		}
+
+		size_t GetReceptiveField() override
+		{
+			return layer1.GetReceptiveField() + layer2.GetReceptiveField();
+		}
+
+		size_t GetNumWeights() override
+		{
+			return layer1.GetNumWeights() + layer2.GetNumWeights();
+		}
+
+		void SetWeights(std::vector<float>::iterator& inWeights) override
+		{
+			layer1.SetWeights(inWeights);
+			layer2.SetWeights(inWeights);
+		}
+
+		void Reset() override
+		{
+			layer1.Reset();
+			layer1Out.SetZero();
+			dLayer1Out.SetZero();
+			layer2.Reset();
+		}
+
+		void DivideWeights(float divisor) override
+		{
+			layer1.DivideWeights(divisor);
+			layer2.DivideWeights(divisor);
+		}
+
+		void ApplyGradients(float scale) override
+		{
+			layer1.ApplyGradients(scale);
+			layer2.ApplyGradients(scale);
+		}
+
+	private:
+		WaveNetLayerBackpropT<T, 1, Channels, 3, 1> layer1;
+		ChannelBuffer<T, Channels, MAX_SAMPLES> layer1Out;
+		WaveNetLayerBackpropT<T, 1, Channels, 3, 2> layer2;
+		ChannelBuffer<T, Channels, MAX_SAMPLES> dLayer1Out;
+		ChannelBuffer<T, Channels, MAX_SAMPLES> headOutput;
+};
+
+int main()
+{
+	//DenseBackpropT<float, 1, 1, false> modelBackprop;
+	//TestModel(modelBackprop);
+
+	//Conv1DBackpropT<float, 1, 1, 3, true, 1> convBackprop;
+	//TestModel(convBackprop);
+
+	//WaveNetLayerBackpropT<float, 1, 3, 1> wn;
+	//TestModel(wn);
+
+	A2BackpropT<float, 1> a2;
+	TestModel(a2);
+
+	//ChainBackpropModelT<float, 1, 1> chainBackProp;
+
+	//auto layer1 = std::make_unique<DenseBackpropT<float, 1, 2, false>>();
+	//auto layer2 = std::make_unique<DenseBackpropT<float, 2, 1, false>>();
+
+	//chainBackProp.AddLayer(std::move(layer1));
+	//chainBackProp.AddLayer(std::move(layer2));
+
+	//TestModel(chainBackProp);
 
 	return 0;
 }
+
--- WaveNetBackprop.h
+++ WaveNetBackprop.h
@@ -4,79 +4,415 @@
 #include "Activation.h"
 #include <array>
 #include <vector>
+#include <format>
 
 using namespace NeuralAudio;
 
 namespace NeuralCpuTrain
 {
-	template <typename T, int InSize, int OutSize, bool DoBias>
-	struct DenseBackpropWeightsT
-	{
-		ChannelBuffer<T, OutSize, InSize> Weights;
-		DenseLayerT<T, InSize, OutSize, DoBias>::BiasType Bias;
-
-		void SetZero()
-		{
-			Weights.SetZero();
-
-			if constexpr (DoBias)
-			{
-				Bias.setZero();
-			}
-		}
-
-		void Divide(float divisor)
-		{
-			auto map = Weights.GetEigenMap();
-
-			map /= divisor;
-
-			if constexpr (DoBias)
-			{
-				Bias /= divisor;
-			}
-		}
-
-		void ApplyGradients(DenseLayerT<T, InSize, OutSize, DoBias>& forwardLayer)
-		{
-			auto map = forwardLayer.GetWeights().GetEigenMap();
-			
-			map.noalias() -= Weights.GetEigenMapConst();
-
-			if constexpr (DoBias)
-			{
-				forwardLayer.GetBias().noalias() -= Bias;
-			}
-		}
-	};
-
-	template <typename T, int InSize, int OutSize, bool DoBias>
-	class DenseBackpropT
+	template <typename T>
+	class BackpropModelBaseT
 	{
 		public:
-			DenseBackpropT(DenseLayerT<T, InSize, OutSize, DoBias>& forwardLayer)
+			BackpropModelBaseT() {}
+
+			virtual ~BackpropModelBaseT() {}
+
+			virtual void Forward(void* input, void* output)
 			{
-				this->forwardLayer = &forwardLayer;
 			}
 
-			void Backward(const ChannelRowSpan<T, InSize>& input,	// forward input data
-				const ChannelRowSpan<T, OutSize>& dOutput,	// incoming backprop gradient
-				const ChannelRowSpan<T, InSize>& dInput,	// outgoing backprop gradient
-				DenseBackpropWeightsT<T, InSize, OutSize, DoBias>& dWeights)	// accumulator for weight gradients
+			virtual size_t GetReceptiveField()
 			{
-				dInput.GetEigenMap().noalias() = forwardLayer->GetWeights().GetEigenMapConst().transpose() * dOutput.GetEigenMapConst();
+				return 0;
+			}
 
-				auto map = dWeights.Weights.GetEigenMap();
+			virtual size_t GetInChannels()
+			{
+				return 0;
+			}
+
+			virtual size_t GetOutChannels()
+			{
+				return 0;
+			}
+
+			virtual size_t GetNumWeights()
+			{
+				return 0;
+			}
+
+			virtual size_t GetNumFeatures()
+			{
+				return 1;
+			}
+
+			virtual void RandomizeWeights()
+			{
+				RandomizeWeights(GetNumFeatures());
+			}
+
+			void RandomizeWeights(size_t numFeatures)
+			{
+				double stddev = std::sqrt(2.0 / (double)numFeatures);
+				std::normal_distribution<double> dist(0.0, stddev);
+				std::mt19937 rng(123);
+
+				size_t numWeights = GetNumWeights();
+
+				std::vector<float> weights(numWeights);
+
+				for (size_t i = 0; i < numWeights; i++)
+				{
+					weights[i] = (float)dist(rng);
+				}
+
+				auto it = weights.begin();
+
+				SetWeights(it);
+			}
+
+			virtual void SetWeights(std::vector<float>::iterator& inWeights)
+			{}
+
+			virtual void Reset()
+			{}
+
+			virtual void DivideWeights(float divisor)
+			{}
+
+			virtual void ApplyGradients(float scale)
+			{}
+
+	};
+
+	template <typename T, int InChannels, int OutChannels>
+	class BackpropModelT : public BackpropModelBaseT<T>
+	{
+		public:
+			void Forward(void* input, void* output) override
+			{
+				Forward(*static_cast<const ChannelRowSpan<T, InChannels>*>(input), *static_cast<const ChannelRowSpan<T, OutChannels>*>(output));
+			}
+
+			virtual void Forward(const ChannelRowSpan<T, InChannels>& input, const ChannelRowSpan<T, OutChannels>& output)
+			{				
+			}
+
+			virtual void Backward(const ChannelRowSpan<T, InChannels>& input,	// forward input data
+				const ChannelRowSpan<T, OutChannels>& dOutput,	// incoming backprop gradient
+				const ChannelRowSpan<T, InChannels>& dInput)	// outgoing backprop gradient
+			{
+			}
+
+			size_t GetNumFeatures() override
+			{
+				return InChannels;
+			}
+
+			size_t GetInChannels() override
+			{
+				return InChannels;
+			}
+
+			size_t GetOutChannels() override
+			{
+				return OutChannels;
+			}
+	};
+
+	template <typename T, int InChannels, int OutChannels>
+	class ChainBackpropModelT : public BackpropModelT<T, InChannels, OutChannels>
+	{
+		public:
+			void AddLayer(std::unique_ptr<BackpropModelBaseT<T>> layer)
+			{
+				if (layers.size() == 0)
+				{
+					if (layer->GetInChannels() != InChannels)
+					{
+						throw std::runtime_error(std::format("First layer should have {} input channels, but has {}.", InChannels, layer->GetInChannels()));
+					}
+				}
+				else
+				{
+					auto& lastLayer = layers.back();
+
+					if (lastLayer->GetOutChannels() != layer->GetInChannels())
+					{
+						throw std::runtime_error(std::format("Layer input channels ({}) does not mach last layer output channesl ({}).", layer->GetInChannels(), lastLayer->GetOutChannels()));
+					}
+				}
+
+				layers.push_back(std::move(layer));
+			}
+
+			void Forward(const ChannelRowSpan<T, InChannels>& input, const ChannelRowSpan<T, OutChannels>& output) override
+			{
+				auto& lastLayer = layers.back();
+
+				if (lastLayer->GetOutChannels() != OutChannels)
+				{
+					throw std::runtime_error(std::format("Last layer should have {} output channels, but has {}.", OutChannels, lastLayer->GetOutChannels()));
+				}
+
+				for (auto& layer : layers)
+				{
+					layer->Forward((void *)&output, (void*)&output);
+				}
+			}
+
+			//void Backward(const ChannelRowSpan<T, InSize>& input,
+			//	const ChannelRowSpan<T, OutSize>& dOutput,
+			//	const ChannelRowSpan<T, InSize>& dInput) override
+			//{
+			//}
+
+		private:
+			std::vector<std::unique_ptr<BackpropModelBaseT<T>>> layers;
+	};
+	
+	template <typename T, int InSize, int OutSize, bool DoBias>
+	class DenseBackpropT : public BackpropModelT<T, InSize, OutSize>
+	{
+		public:
+			DenseBackpropT() :
+				forwardLayer(),
+				dWeights(),
+				dBias()
+			{
+			}
+
+			void Forward(const ChannelRowSpan<T, InSize>& input, const ChannelRowSpan<T, OutSize>& output) override
+			{
+				forwardLayer.Process(input, output);
+			}
+
+			void Backward(const ChannelRowSpan<T, InSize>& input,
+				const ChannelRowSpan<T, OutSize>& dOutput,
+				const ChannelRowSpan<T, InSize>& dInput) override
+			{
+				dInput.GetEigenMap().noalias() = forwardLayer.GetWeights().GetEigenMapConst().transpose() * dOutput.GetEigenMapConst();
+
+				auto map = dWeights.GetEigenMap();
 
 				map.noalias() += dOutput.GetEigenMapConst() * input.GetEigenMapConst().transpose();
 
 				if constexpr (DoBias)
 				{
-					dWeights.Bias.noalias() += dOutput.GetEigenMapConst().rowwise().sum();
+					dBias.noalias() += dOutput.GetEigenMapConst().rowwise().sum();
+				}
+			}
+
+			size_t GetNumWeights() override
+			{
+				return forwardLayer.GetNumWeights();
+			}
+
+			void SetWeights(std::vector<float>::iterator& inWeights) override
+			{
+				forwardLayer.SetWeights(inWeights);
+			}
+
+			void Reset() override
+			{
+				dWeights.SetZero();
+
+				if constexpr (DoBias)
+				{
+					dBias.setZero();
+				}
+			}
+
+			void DivideWeights(float divisor) override
+			{
+				auto map = dWeights.GetEigenMap();
+
+				map /= divisor;
+
+				if constexpr (DoBias)
+				{
+					dBias /= divisor;
+				}
+			}
+
+			void ApplyGradients(float scale) override
+			{
+				auto map = forwardLayer.GetWeights().GetEigenMap();
+
+				map.noalias() -= dWeights.GetEigenMapConst() * scale;
+
+				if constexpr (DoBias)
+				{
+					forwardLayer.GetBias().noalias() -= dBias * scale;
 				}
 			}
 
 		private:
-			DenseLayerT<T, InSize, OutSize, DoBias>* forwardLayer;
+			DenseLayerT<T, InSize, OutSize, DoBias> forwardLayer;
+			ChannelBuffer<T, OutSize, InSize> dWeights;
+			DenseLayerT<T, InSize, OutSize, DoBias>::BiasType dBias;
 	};
+
+	template <typename T, int InChannels, int OutChannels, int KernelSize, bool DoBias, int Dilation>
+	class Conv1DBackpropT : public BackpropModelT<T, InChannels, OutChannels>
+	{
+		public:
+			Conv1DBackpropT()
+			{
+			}
+
+			void Forward(const ChannelRowSpan<T, InChannels>& input, const ChannelRowSpan<T, OutChannels>& output) override
+			{
+				const size_t numFrames = input.GetNumCols();
+
+				for (size_t k = 0; k < KernelSize; k++)
+				{
+					const T* weightPtr = this->weights[k].GetDataConst();
+
+					const auto offset = Dilation * ((int)k + 1 - KernelSize);
+
+					const size_t validSize = numFrames + offset;
+
+					const auto inBlock = input.Slice(0, validSize);
+					const auto outBlock = output.Slice(-offset, validSize);
+
+					outBlock.GetEigenMap().noalias() += weights[k].GetEigenMapConst() * inBlock.GetEigenMapConst();
+				}
+
+				if constexpr (DoBias)
+					output.GetEigenMap().colwise() += bias;
+			}	
+
+			void Backward(const ChannelRowSpan<T, InChannels>& input, const ChannelRowSpan<T, OutChannels>& dOutput, const ChannelRowSpan<T, InChannels>& dInput) override
+			{
+				const size_t numFrames = dOutput.GetNumCols();
+				const auto doutMap = dOutput.GetEigenMap();
+				
+				if constexpr (DoBias)
+				{
+					dBias.noalias() += doutMap.rowwise().sum();
+				}
+
+				for (size_t k = 0; k < KernelSize; ++k)
+				{
+					const T* weightPtr = this->weights[k].GetDataConst();
+
+					const auto offset = Dilation * ((int)k + 1 - KernelSize);
+
+					const size_t validSize = numFrames + offset;
+
+					const auto inBlock = input.Slice(0, validSize);
+					const auto dOutputBlock = dOutput.Slice(-offset, validSize);
+			
+					auto dwMap = dWeights[k].GetEigenMap();
+					dwMap.noalias() += dOutputBlock.GetEigenMapConst() * inBlock.GetEigenMapConst().transpose();
+
+					auto wMap = weights[k].GetEigenMapConst();
+					auto dInputMap = dInput.Slice(-offset, validSize).GetEigenMap();
+					dInputMap.noalias() += wMap.transpose() * dOutputBlock.GetEigenMapConst();
+				}
+			}
+
+			size_t GetNumFeatures() override
+			{
+				return InChannels * KernelSize;
+			}
+
+			size_t GetReceptiveField() override
+			{
+				return (KernelSize - 1) * Dilation;
+			}
+
+			size_t GetNumWeights() override
+			{
+				return OutChannels * InChannels * KernelSize + (DoBias ? OutChannels : 0);
+			}
+
+			void SetWeights(std::vector<float>::iterator& inWeights) override
+			{
+				for (size_t i = 0; i < OutChannels; i++)
+					for (size_t j = 0; j < InChannels; j++)
+						for (size_t k = 0; k < KernelSize; k++)
+							weights[k](i, j) = *(inWeights++);
+
+				if constexpr (DoBias)
+				{
+					for (size_t i = 0; i < OutChannels; i++)
+						bias(i) = *(inWeights++);
+				}
+			}
+
+			void Reset() override
+			{
+				for (int k = 0; k < KernelSize; k++)
+				{
+					dWeights[k].SetZero();
+				}
+
+				if constexpr (DoBias)
+				{
+					dBias.setZero();
+				}
+			}
+
+			void DivideWeights(float divisor) override
+			{
+				for (int k = 0; k < KernelSize; k++)
+				{
+					auto map = dWeights[k].GetEigenMap();
+
+					map /= divisor;
+				}
+
+				if constexpr (DoBias)
+				{
+					dBias /= divisor;
+				}
+			}
+
+			void ApplyGradients(float scale) override
+			{
+				for (int k = 0; k < KernelSize; k++)
+				{
+					auto map = weights[k].GetEigenMap();
+
+					map.noalias() -= dWeights[k].GetEigenMapConst() * scale;
+				}
+
+				if constexpr (DoBias)
+				{
+					bias.noalias() -= dBias * scale;
+				}
+			}
+
+		private:
+			alignas(32) std::array<ChannelBuffer<T, OutChannels, InChannels>, KernelSize> weights;
+			Eigen::Vector<T, OutChannels> bias;
+			alignas(32) std::array<ChannelBuffer<T, OutChannels, InChannels>, KernelSize> dWeights;
+			Eigen::Vector<T, OutChannels> dBias;
+	};
+
+	template <typename T, int Channels>
+	class LeakyReLUT : public BackpropModelT<T, Channels, Channels>
+	{
+		public:
+			void Forward(const ChannelRowSpan<T, Channels>& input, const ChannelRowSpan<T, Channels>& output) override
+			{
+				auto outputMap = output.GetEigenMap();
+				auto inputMap = input.GetEigenMapConst();
+
+				outputMap = (inputMap.array() < TCONST(0.0)).select(inputMap.array() * TCONST(0.01), inputMap.array());
+			}
+
+			void Backward(const ChannelRowSpan<T, Channels>& input, const ChannelRowSpan<T, Channels>& dOutput, const ChannelRowSpan<T, Channels>& dInput) override
+			{
+				auto inputMap = input.GetEigenMapConst();
+				auto dOutputMap = dOutput.GetEigenMapConst();
+				auto dInputMap = dInput.GetEigenMap();
+
+				dInputMap = (inputMap.array() < TCONST(0.0)).select(dOutputMap.array() * TCONST(0.01), dOutputMap.array());
+			}
+	};
+
 } // namespace NeuralCpuTrain
